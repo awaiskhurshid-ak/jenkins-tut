b1c188c6ea4645c84e53ac30b2ba95928c486603
Awais <awaiskhurshid.ak@gmail.com>
2023-02-16 17:36:15 +0500

    again update

--- script.groovy
+++ script.groovy
@@ -7,7 +7,7 @@ def buildImage() {
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
         sh 'docker build -t nanajanashia/demo-app:jma-2.0 .'
-        sh "echo $PASS | docker login -u $USER --password-stdin"
+        sh "docker login -u $USER -p $PASS"
         sh 'docker push nanajanashia/demo-app:jma-2.0'
     }
 } 
