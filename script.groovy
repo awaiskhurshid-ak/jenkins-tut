6f1e3890b400bb5838f0265315f8f926af3c1e4d
awaiskhurshid-ak <73350770+awaiskhurshid-ak@users.noreply.github.com>
2023-02-16 19:40:31 +0500

    jejoi

--- script.groovy
+++ script.groovy
@@ -5,9 +5,9 @@ def buildJar() {
 
 def buildImage() {
     echo "building the docker image..."
-    sh 'docker build -t awaiskhurshid/java-maven-app:jma-3.0 .'
+    
     withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        
+        sh 'docker.build -t awaiskhurshid/java-maven-app:jma-3.0 .'
         sh "docker login -u $USER -p $PASS"
         sh 'docker push awaiskhurshid/java-maven-app:jma-3.0'
     }
